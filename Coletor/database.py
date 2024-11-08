c9c38b79cfcd598e380133a6582e37e509275711
juanpythonvolei <juanzsalca@outlook.com>
2024-11-08 00:26:46 -0300

    Update database.py

--- Coletor/database.py
+++ Coletor/database.py
@@ -177,5 +177,5 @@ class EansProdutos(Base):
         self.codigo_ean = codigo_ean
         self.produto = produto
 
-engine = create_engine(st.secrets['database'])
+engine = create_engine('sqlite:///database/estoque.db')
 base =Base.metadata.create_all(engine)
