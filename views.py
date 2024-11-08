07a28a094297591f03ce619313e3ad7104377ffa
juanpythonvolei <juanzsalca@outlook.com>
2024-11-08 14:57:21 -0300

    Update views.py

--- views.py
+++ views.py
@@ -103,7 +103,7 @@ def carregar_arquivo(pergunta,conteudo):
 def confirmar(function):
     confirmar = st.button('Confirmar')
     if confirmar:
-       function
+       function()
 
 def save_table_int_text(table,user,data):
     tabela = table.to_string()
