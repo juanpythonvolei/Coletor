b3e65eaefa1677e40c8d9d9cb851e5710ea83ff7
juanpythonvolei <juanzsalca@outlook.com>
2024-11-08 14:54:22 -0300

    Update Faturamento.py

--- pages/Faturamento.py
+++ pages/Faturamento.py
@@ -16,12 +16,12 @@ with coly:
 
 uploaded_files = st.file_uploader("Seleção", type=[f'xml'], accept_multiple_files=True,help='Insira suas notas aqui')
 if uploaded_files:
-            
         resultado = confirmar(process_notes(notes_list=uploaded_files,data=str(date.today()),usuario=st.session_state.selected_option,status=False,peso_recebido=float(1)))
-        col1,col2,col3 = st.columns(3)
-        with col1:
-              st.metric(label="Notas Faturadas",value=resultado[0])
-        with col2:
-              st.metric(label="Notas não faturadas",value=resultado[2])
-        with col3:
-              st.metric(label='Cadastros',value=resultado[1])
+        if resultado:
+              col1,col2,col3 = st.columns(3)
+              with col1:
+                    st.metric(label="Notas Faturadas",value=resultado[0])
+              with col2:
+                    st.metric(label="Notas não faturadas",value=resultado[2])
+              with col3:
+                    st.metric(label='Cadastros',value=resultado[1])
