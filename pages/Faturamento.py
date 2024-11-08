55034497233a98b0e482c9e7e3e9e959445b1012
juanpythonvolei <juanzsalca@outlook.com>
2024-11-08 15:05:36 -0300

    Update Faturamento.py

--- pages/Faturamento.py
+++ pages/Faturamento.py
@@ -17,8 +17,9 @@ with coly:
 uploaded_files = st.file_uploader("Seleção", type=[f'xml'], accept_multiple_files=True,help='Insira suas notas aqui')
 if uploaded_files:
         alert = confirmar()     
+        st.write(alert)
         if alert:
-              st.write(alert)
+              
               resultado = process_notes(notes_list=uploaded_files,data=str(date.today()),usuario=st.session_state.selected_option,status=False,peso_recebido=float(1))
               if resultado:
                     col1,col2,col3 = st.columns(3)
