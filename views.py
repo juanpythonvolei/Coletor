e6ee377793234e055183ddc32d070be51cf2ff5b
juanpythonvolei <juanzsalca@outlook.com>
2024-11-09 17:31:21 -0300

    Update views.py

--- views.py
+++ views.py
@@ -118,20 +118,11 @@ def carregar_arquivo(pergunta,conteudo):
     arquivos_upload = []
     texto = ''
     for item in conteudo:
-        if 'pdf' in item.name:
-            mime_type = "application/pdf"
-        elif 'xlsx' in item.name:
             tabela = treat_table(item)
             texto += f'{tabela}\n'
-        elif 'imagem' in item.name:
-            mime_type ="image/png" 
-        else:
-            raise ValueError("Tipo de arquivo não suportado")
-    st.write(texto)
-    arquivo = {"text":texto}       
     genai.configure(api_key=st.secrets['ia']) 
     model = genai.GenerativeModel('gemini-1.5-flash') 
-    chat = model.start_chat(history=[{"role":"user","parts":[arquivo]}]) 
+    chat = model.start_chat(history=[{"role":"user","parts":[{"text":texto}]}]) 
     response = chat.send_message(pergunta) 
     return response.text
 
