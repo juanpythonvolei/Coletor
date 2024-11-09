8aaa5e91942be6b6c73b6977774df75e53a19360
juanpythonvolei <juanzsalca@outlook.com>
2024-11-09 12:32:04 -0300

    Update views.py

--- views.py
+++ views.py
@@ -359,7 +359,6 @@ def picking(data):
     verificar_se_ativo = session.query(Faturamento).filter(Faturamento.data == data,Faturamento.status == False).all()
     se_mercado_concluido = session.query(Picklist.nota,Picklist.produto).filter(Picklist.data == str(date.today()),Picklist.status == True).all()
     lista_conferencia = [elemento for elemento in se_mercado_concluido]
-    st.write(lista_conferencia)
     for item in list(set(verificar_se_ativo)):
         try:
             if (item.numero_da_nota,item.produto) in lista_conferencia:
