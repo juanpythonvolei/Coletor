3d59c2b624d522136924799d0bf9ff7f8031b64d
juanpythonvolei <juanzsalca@outlook.com>
2024-11-09 20:22:13 -0300

    Update Rotas.py

--- pages/Rotas.py
+++ pages/Rotas.py
@@ -1,2 +1,16 @@
 import streamlit as st 
 from views import *
+
+image = st.image('https://img.freepik.com/vetores-gratis/modelo-de-logotipo-da-empresa-de-caminhoes_441059-258.jpg?w=996')
+
+colx,coly,colz = st.columns(3)
+
+with colx:
+      if 'selected_option' in st.session_state:
+            st.title(f'Usuário logado: {st.session_state.selected_option}')
+with coly:
+      with st.popover('🤖'):
+            assistant()
+with colz:
+      with st.popover('📂'):
+            donwload_billing()
