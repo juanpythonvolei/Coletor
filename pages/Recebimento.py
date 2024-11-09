47db26254ed4242ff6c2447d57c45d544422f84e
juanpythonvolei <juanzsalca@outlook.com>
2024-11-09 11:37:53 -0300

    Update Recebimento.py

--- pages/Recebimento.py
+++ pages/Recebimento.py
@@ -1,6 +1,8 @@
 import streamlit as st 
 from views import *
 
+image = st.image('https://img.freepik.com/vetores-gratis/modelo-de-logotipo-da-empresa-de-caminhoes_441059-258.jpg?w=996')
+
 colx,coly,colz = st.columns(3)
 
 with colx:
