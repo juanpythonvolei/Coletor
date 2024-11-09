4110ad2bcc6b6b7c9039d94b37c3c5528b7b03d0
juanpythonvolei <juanzsalca@outlook.com>
2024-11-09 11:37:14 -0300

    Update Assistente.py

--- pages/Assistente.py
+++ pages/Assistente.py
@@ -1,4 +1,4 @@
 import streamlit as st
 from views import *
-
+image = st.image('https://img.freepik.com/vetores-gratis/modelo-de-logotipo-da-empresa-de-caminhoes_441059-258.jpg?w=996')
 assistant()
