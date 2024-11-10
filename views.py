91e6911ee3b1117ffbbccfee3607a00083273e72
juanpythonvolei <juanzsalca@outlook.com>
2024-11-10 13:52:56 -0300

    Update views.py

--- views.py
+++ views.py
@@ -841,6 +841,7 @@ def calculate_distance(destiny):
                 'key': st.secrets['chave_api_googlemaps']
                 }
         response = requests.get(url, params=params)
+        st.info(response)
         if response.status_code == 200:
             data = response.json()
             route = data['routes'][0]
