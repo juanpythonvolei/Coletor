98e90437caf46557ad6b8f99cdfcf1a86609e43b
juanpythonvolei <juanzsalca@outlook.com>
2024-11-10 11:39:50 -0300

    Update views.py

--- views.py
+++ views.py
@@ -888,7 +888,7 @@ def route(list):
 def build_google_map(list):
     base_url = "https://www.google.com/maps/dir/"
     final=base_url + '/'.join([f'{item['descricao']}' for item in list])
-    final = final.strip()
+    final = str(final).strip()
     return st.info(final)
     
     
