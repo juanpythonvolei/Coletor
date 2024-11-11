8aaba1b0b0a831f2665f41b028789969d7890faf
Juan Pablo <juanzsalca@outlook.com>
2024-11-11 19:33:12 -0300

    Update Frota.py

--- pages/Frota.py
+++ pages/Frota.py
@@ -1,4 +1,31 @@
 import streamlit as st
 from views import *
 
+image = st.image('https://img.freepik.com/vetores-gratis/modelo-de-logotipo-da-empresa-de-caminhoes_441059-258.jpg?w=996')
+
+colx,coly,colz = st.columns(3)
+
+with colx:
+      if 'selected_option' in st.session_state:
+            st.title(f'Usuário logado: {st.session_state.selected_option}')
+with coly:
+      with st.popover('🤖'):
+            assistant()
+with colz:
+      with st.popover('📂'):
+            donwload_billing()
+
+
+marca = st.text_input(label="Marca",palceholder="Insira a marca do veículo")
+modelo = st.text_input(label="Modelo",palceholder="Insira o modelo do veículo")
+autonomia = st.number_input(label="Autonomia",palceholder="Insira a Autonomia do veículo",value=None)
+
+if marca and modelo and autonomia:
+  try:
+    session.query(Veiculos).filter(Veiculos.modelo == modelo).first()
+    return st.error(f'O veículo modelo: {modelo} já existe')
+  except:
+    session.add(Veiculos(marca=marca,modelo=modelo,autonomia=autonomia))
+    return st.success(f'O veículo modelo: {modelo} foi cadastrado com sucesso')
+
 
