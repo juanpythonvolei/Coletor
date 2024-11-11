36c3e26b93418391359b6063cea2c2d8ce4a2b8d
Juan Pablo <juanzsalca@outlook.com>
2024-11-11 16:56:30 -0300

    Update Rotas.py

--- pages/Rotas.py
+++ pages/Rotas.py
@@ -87,4 +87,5 @@ with tabd:
                   if transp:
                         notas = st.multiselect(label="notas",placeholder="Selecione uma nota",options=list(set([item[0] for item in session.query(Faturamento.numero_da_nota).filter(Faturamento.status == True,Faturamento.transportadora == transp).all()])),key='select_notes_deli')    
                         if notas: 
+                              St.title('Entregas')
                               load_delivery(notas,data)
