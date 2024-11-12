5ed08729a814432e4b4d06d0eeff73e822b3150c
Juan Pablo <juanzsalca@outlook.com>
2024-11-12 15:10:48 -0300

    Update Rotas.py

--- pages/Rotas.py
+++ pages/Rotas.py
@@ -2,10 +2,11 @@ import streamlit as st
 from views import *
 
 image = st.image('https://img.freepik.com/vetores-gratis/modelo-de-logotipo-da-empresa-de-caminhoes_441059-258.jpg?w=996')
-
-taba,tabb,tabc,tabd,tabe = st.tabs(['Ver rotas','Rotas espefíficas','Roteiro','Entregas','Visão Geral'])
+try:
+      logado = st.session_state.selected_option
+      taba,tabb,tabc,tabd,tabe = st.tabs(['Ver rotas','Rotas espefíficas','Roteiro','Entregas','Visão Geral'])
       
-if taba:
+      if taba:
             with taba:
                   colx,coly,colz = st.columns(3)
                   
@@ -33,7 +34,7 @@ if taba:
                         save_route(data=data,transp=transp,routes=result[2])
                         st.table(result[1])
                         st.link_button(label="Acessar Rota",url=result[0])
-with tabb:
+      with tabb:
                   data = st.date_input("Selecione uma data",value=None,key='Data_selector_epecific')
                   if data:
                         transp = st.selectbox(label="Trasnportadora",placeholder="Selecione uma transportadora",options=list(set([item[0] for item in session.query(Faturamento.transportadora).filter(Faturamento.status == True).all()])),index=None,key='select_transp')    
@@ -54,7 +55,7 @@ with tabb:
                                     st.link_button(label="Acessar Rota",url=result[0])
                                     save_route(data=data,transp=transp,routes=result[2])
                         
-with tabc:
+      with tabc:
             texto = ''
             data = st.date_input("Selecione uma data",value=None,key='Data_selector_ia')
             if data:
@@ -82,7 +83,7 @@ with tabc:
                                     response = analisar(f"Analise o texto que você está recebendo. Ele é uma representação de rotas. Nele estão os destinos, e o nome dos clientes. Oraganize-os e, baseando-se nele, responda ao que se pede retornando além da resposta, uma rota organizada: {message}",str(texto))
                                     assistant.write(response)
                                     
-with tabd:
+      with tabd:
             data = st.date_input("Selecione uma data",value=None,key='Data_selector_deli')
             if data:
                         veiculo = st.selectbox(label="Veículo",placeholder="Selecione um veículo",options=list(set([item[0] for item in session.query(Veiculos.modelo).all()])),index=None,key='select_car_deli')
@@ -102,7 +103,7 @@ with tabd:
                                           response = load_delivery(notas,data,veiculo)
                                           st.metric('Entregas não completas',response[0])
                                           st.metric('Entregas completas',response[1])
-with tabe:
+      with tabe:
             verificar = session.query(Entregas.data).filter(Entregas.status==True).all()
             data = st.date_input("Selecione uma data",value=None,key='Data_selector_one')
             if data:
@@ -113,4 +114,5 @@ with tabe:
                         st.metric('Total Gasto R$',value=response[1])
                         st.metric('Total Percorrido Km',value=response[2])
                         st.metric('Quantidade total entregue',value=response[3])
-
+except:
+      st.error('Você deve estar logado para acessar essa página')
