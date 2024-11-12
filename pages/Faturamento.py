54bbb85c65d5f55a3a0c6b84b30454fe9c182f4c
Juan Pablo <juanzsalca@outlook.com>
2024-11-12 14:08:32 -0300

    Update Faturamento.py

--- pages/Faturamento.py
+++ pages/Faturamento.py
@@ -17,9 +17,12 @@ try:
       with colz:
             with st.popover('📂'):
                   donwload_billing()
-
-      uploaded_files = st.file_uploader("Seleção", type=[f'xml'], accept_multiple_files=True,help='Insira suas notas aqui',key='Faturamento')
-      if uploaded_files:
+      try:
+            session.query(Faturamento).filter(Faturamento.status == False).first()
+            st.warnig('Aind há notas a serem fatuardas')
+      except:
+            uploaded_files = st.file_uploader("Seleção", type=[f'xml'], accept_multiple_files=True,help='Insira suas notas aqui',key='Faturamento')
+            if uploaded_files:
                           resultado = process_notes(notes_list=uploaded_files,data=str(date.today()),usuario=st.session_state.selected_option,status=False,peso_recebido=float(1))
                           if resultado:
                                 col1,col2,col3 = st.columns(3)
