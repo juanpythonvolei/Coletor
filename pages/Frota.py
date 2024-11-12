ddfca52e91b01c1e7e3abdd43b2f7966c81d3c6e
Juan Pablo <juanzsalca@outlook.com>
2024-11-12 11:13:58 -0300

    Update Frota.py

--- pages/Frota.py
+++ pages/Frota.py
@@ -14,61 +14,63 @@ with coly:
 with colz:
       with st.popover('📂'):
             donwload_billing()
-
-with st.popover('Cadastrar veículos 🚚'):
-      marca = st.text_input(label="Marca",placeholder="Insira a marca do veículo")
-      modelo = st.text_input(label="Modelo",placeholder="Insira o modelo do veículo")
-      autonomia = st.number_input(label="Autonomia",placeholder="Insira a Autonomia do veículo",value=None)
+if st.session_state.selected_option == None:
+      st.error('Você deve estar logado para acessar essa página')
+else:
+      with st.popover('Cadastrar veículos 🚚'):
+            marca = st.text_input(label="Marca",placeholder="Insira a marca do veículo")
+            modelo = st.text_input(label="Modelo",placeholder="Insira o modelo do veículo")
+            autonomia = st.number_input(label="Autonomia",placeholder="Insira a Autonomia do veículo",value=None)
+            
+            if marca and modelo and autonomia:
+                verificar = session.query(Veiculos).filter(Veiculos.modelo==modelo).first()
+                if verificar:
+                      st.error(f'O modelo: {verificar.modelo} já está cadastrado')
+                else:  
+                      session.add(Veiculos(marca=marca,modelo=modelo,autonomia=autonomia))
+                      session.commit()    
+                      st.success(f'O veículo modelo: {modelo} foi cadastrado com sucesso')
       
-      if marca and modelo and autonomia:
-          verificar = session.query(Veiculos).filter(Veiculos.modelo==modelo).first()
-          if verificar:
-                st.error(f'O modelo: {verificar.modelo} já está cadastrado')
-          else:  
-                session.add(Veiculos(marca=marca,modelo=modelo,autonomia=autonomia))
-                session.commit()    
-                st.success(f'O veículo modelo: {modelo} foi cadastrado com sucesso')
-
-botao_mudar = st.popover('Alterar informações ⚙️')
-with botao_mudar:
-      veiculo = st.selectbox(label='Seleção',placeholder="Selecione o veículo",options=[item[0] for item in session.query(Veiculos.modelo).all()],key='escolha',index=None)
-      if veiculo:
-            alteracao = st.selectbox(label='Seleção',placeholder="Selecione o veículo",options=['Marca','Modelo','Autonomia'],key='alteração',index=None)
-            if alteracao:
-                  valor = st.text_input(label='Alteração',placeholder='Insira sua alteração')
-                  if valor:
+      botao_mudar = st.popover('Alterar informações ⚙️')
+      with botao_mudar:
+            veiculo = st.selectbox(label='Seleção',placeholder="Selecione o veículo",options=[item[0] for item in session.query(Veiculos.modelo).all()],key='escolha',index=None)
+            if veiculo:
+                  alteracao = st.selectbox(label='Seleção',placeholder="Selecione o veículo",options=['Marca','Modelo','Autonomia'],key='alteração',index=None)
+                  if alteracao:
+                        valor = st.text_input(label='Alteração',placeholder='Insira sua alteração')
+                        if valor:
+                              verificar = session.query(Veiculos).filter(Veiculos.modelo==veiculo).first()
+                              if alteracao == 'Marca':
+                                    verificar.marca = valor
+                                    session.commit()
+                                    st.success(f'Alteração no campo {alteracao} para o veículo: {veiculo} realizada com sucesso')
+                              elif alteracao == 'Modelo':
+                                    verificar.modelo = valor
+                                    session.commit()
+                                    st.success(f'Alteração no campo {alteracao} para o veículo: {veiculo} realizada com sucesso')
+                              elif alteracao == 'Autonomia':
+                                    verificar.autonomia = float(valor)
+                                    session.commit()
+                                    st.success(f'Alteração no campo {alteracao} para o veículo: {veiculo} realizada com sucesso')
+                        
+      botao_excluir = st.popover('Excluir informações 🗑️')
+      with botao_excluir:
+            veiculo = st.selectbox(label='Seleção',placeholder="Selecione o veículo",options=[item[0] for item in session.query(Veiculos.modelo).all()],key='deletar',index=None)
+            if veiculo:
+                  senha = st.text_input(label='Insira a senha do administrador');
+                  if senha == '1020':
+                        session.delete(session.query(Veiculos).filter(Veiculos.modelo==veiculo).firts())
+                        session.commit()
+                        st.success(f'O Veículo de modelo: {veiculo} foi deletado com sucesso')
+      
+      with st.popover('Ver Frota 🔍'):
+            veiculo = st.selectbox(label='Seleção',placeholder="Selecione o veículo",options=[item[0] for item in session.query(Veiculos.modelo).all()],key='ver',index=None)
+            if veiculo:
                         verificar = session.query(Veiculos).filter(Veiculos.modelo==veiculo).first()
-                        if alteracao == 'Marca':
-                              verificar.marca = valor
-                              session.commit()
-                              st.success(f'Alteração no campo {alteracao} para o veículo: {veiculo} realizada com sucesso')
-                        elif alteracao == 'Modelo':
-                              verificar.modelo = valor
-                              session.commit()
-                              st.success(f'Alteração no campo {alteracao} para o veículo: {veiculo} realizada com sucesso')
-                        elif alteracao == 'Autonomia':
-                              verificar.autonomia = float(valor)
-                              session.commit()
-                              st.success(f'Alteração no campo {alteracao} para o veículo: {veiculo} realizada com sucesso')
-                  
-botao_excluir = st.popover('Excluir informações 🗑️')
-with botao_excluir:
-      veiculo = st.selectbox(label='Seleção',placeholder="Selecione o veículo",options=[item[0] for item in session.query(Veiculos.modelo).all()],key='deletar',index=None)
-      if veiculo:
-            senha = st.text_input(label='Insira a senha do administrador');
-            if senha == '1020':
-                  session.delete(session.query(Veiculos).filter(Veiculos.modelo==veiculo).firts())
-                  session.commit()
-                  st.success(f'O Veículo de modelo: {veiculo} foi deletado com sucesso')
-
-with st.popover('Ver Frota 🔍'):
-      veiculo = st.selectbox(label='Seleção',placeholder="Selecione o veículo",options=[item[0] for item in session.query(Veiculos.modelo).all()],key='ver',index=None)
-      if veiculo:
-                  verificar = session.query(Veiculos).filter(Veiculos.modelo==veiculo).first()
-                  response = complete_desciption(veiculo)
-                  st.info(f'''
-                  Marca: {verificar.marca}\n
-                  autonomia: {verificar.autonomia} km/l\n
-                  Total gasto: R$ {response[0]}\n
-                  Total percorrido: {response[1]} Km
-                  ''')
+                        response = complete_desciption(veiculo)
+                        st.info(f'''
+                        Marca: {verificar.marca}\n
+                        autonomia: {verificar.autonomia} km/l\n
+                        Total gasto: R$ {response[0]}\n
+                        Total percorrido: {response[1]} Km
+                        ''')
