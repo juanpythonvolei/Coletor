644af5d7c6d2f84a8cf8ad380a057be9139e26e4
Juan Pablo <juanzsalca@outlook.com>
2024-11-12 16:40:16 -0300

    Update Rotas.py

--- pages/Rotas.py
+++ pages/Rotas.py
@@ -2,6 +2,10 @@ import streamlit as st
 from views import *
 
 image = st.image('https://img.freepik.com/vetores-gratis/modelo-de-logotipo-da-empresa-de-caminhoes_441059-258.jpg?w=996')
+
+if 'selected_option' in st.session_state:
+            st.title(f'Usuário logado: {st.session_state.selected_option}')
+      
 try:
       logado = st.session_state.selected_option
       taba,tabb,tabc,tabd,tabe = st.tabs(['Ver rotas','Rotas espefíficas','Roteiro','Entregas','Visão Geral'])
