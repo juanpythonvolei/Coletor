fd96e0cb28685535661d66e1b79d6239ebfa83cf
Juan Pablo <juanzsalca@outlook.com>
2024-11-12 20:55:16 -0300

    Update views.py

--- views.py
+++ views.py
@@ -49,34 +49,46 @@ def new_logged_infos(user):
     eigth.empty()
     nineth = st.subheader('''4. 
     Agora, você já pode ir para aba de faturamento, e realizar o faturamento manual. Assim, preencha todos os
-    campos e, ao fim, o faturamento terá se iniciado. obs: Caso você queira realizar o faturamento com arquivos de notas fiscais, clique no icone maior
-    a primeira ação que ele fará é, automaticamente, cadastrar os itens da nota. Depois, você deve realizar o processo de recebimento e transferência. q
+    campos e, ao fim, o faturamento terá se iniciado. obs: Caso você queira realizar o faturamento com arquivos de 
+    notas  fiscais 
+    clique no icone maior
+    a primeira ação que ele fará é, automaticamente, cadastrar os itens da nota. Depois, você deve realizar o processo de
+    recebimento e transferência.
     Quando concluir, é só retornar no icone e reselecionar as notas. Isso fará com que o Faturamento se inicie.
     ''')
     sleep(6)
     nineth.empty()
     tenth = st.subheader('''
     5. Entre na aba de Pickinglist. Para coletar os itens, observe sua localização e quantidade. A coleta se da na ação
-    de inserir o código do produto no campo. Se houver apenas 1 item, o processo se encerrará. Se não, o mostrador irá mudar até que você colete
-    a quantidade da nota. Se completar esse campo, vá para Separação. Nessa aba, é o mesmo procedimento porém, além do item, deve-se inserir a posição dele
+    de inserir o código do produto no campo. Se houver apenas 1 item, o processo se encerrará. Se não, o mostrador irá mudar 
+    até que você colete
+    a quantidade da nota. Se completar esse campo, vá para Separação. Nessa aba, é o mesmo procedimento porém, além do item
+    deve-se inserir a posição dele
     no estoque para contabilizar uma coleta
     ''')
     sleep(6)
     tenth.empty()
     eleventh = st.subheader('''
-    6.Pronto, seu faturamento acabou. Agora, vá para Rotas e depois em entregas. Nessa aba selecione a data de hoje, a transportadora e um carro de tranporte.
-    Após isso, serão exibidos todos os pedidos faturados e separados. Para entregar, é só ativar a chave. Nesse caso,você assume o papel do entregador. Ops, esqueci. O carro utilizado também
-    deve ser cadastrado. Para isso, vá para a aba "Frota" e lá, preencha os dados do veículo. Eles são importantes para as informações vindas da entrega. 
+    6.Pronto, seu faturamento acabou. Agora, vá para Rotas e depois em entregas. Nessa aba selecione a data de hoje, a 
+    transportadora e  um carro de tranporte.
+    Após isso, serão exibidos todos os pedidos faturados e separados. Para entregar, é só ativar a chave. 
+    Nesse caso,você assume o papel do entregador. Ops, esqueci. O carro utilizado também
+    deve ser cadastrado. Para isso, vá para a aba "Frota" e lá, preencha os dados do veículo. Eles são importantes para as informações 
+    vindas da entrega. 
     ''')
     sleep(6)
     eleventh.empty()
     twelveth = st.subheader('''7. 
     Pronto!, Esse é o fluxo. Além, dele, você pode acessar diversos outros menus de consulta, falar com o assitente inteligente, ver o histórico etc.
-    O assistente possúi uma aba dedicada, mas está em todos os menus na forma de um robozinho. Ele sabe tudo sobre as movimentações feitas no aplicativo, então se precisar de uma esclarecimento[
+    O assistente possúi uma aba dedicada, mas está em todos os menus na forma de um robozinho. Ele sabe tudo sobre as movimentações feitas no aplicativo
+    então se precisar de uma esclarecimento
     ou algo do gênero, pode falar com ele.
 
-    Obs: Este app, não foi desenvolvido para uso empresarial. Por conta disso, algumas etapas, como o faturamento, não podem ser equiparadas a um faturamento real visto que careço da posse de sistemas
-    e credenciais que somente estão em posse de empresa. Outro ponto importante é que ele está em constante desenvolvimento, então algumas funções melhorarão ou serão substituidas e até acrescentadas.
+    Obs: Este app, não foi desenvolvido para uso empresarial. Por conta disso, algumas etapas, como o faturamento, não podem ser equiparadas a um 
+    faturamento real visto que careço 
+    da posse de sistemas
+    e credenciais que somente estão em posse de empresa. 
+    Outro ponto importante é que ele está em constante desenvolvimento, então algumas funções melhorarão ou serão substituidas e até acrescentadas.
     ''')
     sleep(6)
     twelveth.empty()
