2be9dfbd18ba5f89354f73ad1d719c7b9ae29340
Juan Pablo <juanzsalca@outlook.com>
2024-11-12 12:08:59 -0300

    Update Menu.py

--- pages/Menu.py
+++ pages/Menu.py
@@ -73,6 +73,18 @@ try:
             with pop_over('Deletar_tabelas'):
                   senha = text_input(label='Senha',placeholder='Insira a senha do administrador',key='tabs') 
                   if senha == '1020':
+                            session.query(Estoque).delete()  
+                            session.query(Faturamento).delete()  
+                            session.query(Picklist).delete()  
+                            session.query(Separacao).delete()  
+                            session.query(Romaneios).delete()  
+                            session.query(Historico).delete()  
+                            session.query(Usuarios).delete()  
+                            session.query(Recebimento).delete()  
+                            session.query(Produtos).delete()  
+                            session.query(Entregas).delete()  
+                            session.query(Veiculos).delete()  
+                            st.success('Todas as tabelas foram excluidas com sucesso')  
                         
 except:
       st.error('Você deve estar logado para acessar essa página')
