dc833454e93625baf897cf5f8ef39e43f60f55b4
Juan Pablo <juanzsalca@outlook.com>
2024-11-12 16:25:40 -0300

    Update Assistente.py

--- pages/Assistente.py
+++ pages/Assistente.py
@@ -4,8 +4,5 @@ from views import*
 
 
 image = st.image('https://img.freepik.com/vetores-gratis/modelo-de-logotipo-da-empresa-de-caminhoes_441059-258.jpg?w=996')
-try:
-      logado = st.session_state.selected_option
-      assistant()
-except:
-  st.error('Você deve estar logado para acessar essa pagina')
+
+assistant()
