e55b488b0073845a42e5fe8f3bc6ebaafd0db0da
Juan Pablo <juanzsalca@outlook.com>
2024-11-12 15:34:28 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1033,7 +1033,7 @@ def complete_desciption(car):
             origem =  destino
         return gasto,distancia_per,qtd
     
-def manual_billing(code,price,transp,client,data,user,qtd,number,status,data_emition,peso,description,destino):
+def manual_billing(code,transp,client,user,qtd,number,destino):
                     verificar = session.query(Produtos).filter(Produtos.codigo == code).first()
                     if verificar:
                         posicao = session.query(Estoque).filter(Estoque.item == verificar.codigo,Estoque.quantidade >= qtd).first().endereco
@@ -1041,8 +1041,9 @@ def manual_billing(code,price,transp,client,data,user,qtd,number,status,data_emi
                             if session.query(Estoque).filter(Estoque.item == verificar.codigo,Estoque.endereco == posicao).first().quantidade >= qtd:
                                 session.query(Estoque).filter(Estoque.item == verificar.codigo,Estoque.endereco == posicao).first().quantidade -= qtd
                                 session.commit()
-                                session.add(Faturamento(produto=verificar.codigo,usuario=user,quantidade=qtd,numero_da_nota=number,data=data,status=status,transportadora=transp,cliente=client,data_emissao=data_emition,posicao=posicao,destino=destino))
+                                session.add(Faturamento(produto=verificar.codigo,usuario=user,quantidade=qtd,numero_da_nota=number,data=str(date.today()),status=False,transportadora=transp,cliente=client,data_emissao=str(dat.today()),posicao=posicao,destino=destino))
                                 session.commit()
                                 add_history(action=f"Faturamento",qtd=qtd,data=data,item=code,user=user)
                                 verify_if_still_exists(code=code,adress=posicao)
                                 session.commit()
+                                st.success(f"O Item {code}, foi faturado com sucesso e já está disponível para ser coletado")
