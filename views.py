4fc18b37d0948ecc51c9c446f9f8ac6c5183b1bf
Juan Pablo <juanzsalca@outlook.com>
2024-11-11 22:00:03 -0300

    Update views.py

--- views.py
+++ views.py
@@ -929,8 +929,7 @@ def complete_delivery(data,transp):
         lista = []
         verificar = session.query(Entregas).filter(Entregas.data==data,Entregas.transportadora == transp,Entregas.status==True).all()
         for i,item in enumerate(list(set(verificar))):
-            st.write(int(item.nota))
-            destino = session.query(Faturamento).filter(Faturamento.status==True,Faturamento.numero_da_nota==int(item.nota)).first().destino
+            destino = session.query(Faturamento).filter(Faturamento.status==True).first().destino
             distancia = build_google_map(route(define_destiny_list([destino])))[2][0]['Distância']
             kml = session.query(Veiculos).filter(Veiculo.modelo==item.modelo).first().autonomia
             dict={
