6451e54b2eaf669a44ac0aecbb394360242fc409
Juan Pablo <juanzsalca@outlook.com>
2024-11-12 11:18:09 -0300

    Update Assistente.py

--- pages/Assistente.py
+++ pages/Assistente.py
@@ -1,4 +1,11 @@
 import streamlit as st
-from views import *
+from views import* 
+
+
+
 image = st.image('https://img.freepik.com/vetores-gratis/modelo-de-logotipo-da-empresa-de-caminhoes_441059-258.jpg?w=996')
-assistant()
+try:
+      logado = st.session_state.selected_option
+      assistant()
+except:
+  st.error('Você deve estar logado para acessar essa pagina')
