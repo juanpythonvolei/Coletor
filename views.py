f54252fe97f5effadf164ec9f752d8a53e454819
Juan Pablo <juanzsalca@outlook.com>
2024-11-15 13:42:19 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1140,8 +1140,7 @@ def complete_delivery(data,transp):
                     lista.append(dict)
                 origem = destino
         else:
-            verificar = session.query(Entregas).filter(Entregas.data==data,Entregas.transportadora == transp,Entregas.status==True).first()
-            st.write(verificar)
+            verificar = session.query(Entregas).filter(Entregas.data==data,Entregas.transportadora == transp,Entregas.status==True).first().produto
             destino = session.query(Faturamento).filter(Faturamento.status==True,Faturamento.numero_da_nota == verificar.nota,Faturamento.data==verificar.data).first().destino
             verificar_car = session.query(Entregas).filter(Entregas.data==data,Entregas.transportadora == transp,Entregas.status==True).first().veiculo
             autonomia_itupeva = session.query(Veiculos).filter(Veiculos.modelo == verificar_car).first().autonomia
